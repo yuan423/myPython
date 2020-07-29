7d8a07035e4efb7eaddac32e08378d62126d0177
chang yuan <965211767@qq.com>
2020-07-29 13:37:53 +0800

    python

---
+++ BiaoShiFu.py
@@ -0,0 +1,17 @@
+# 获取下python中所有的关键字
+
+import keyword
+print(keyword.kwlist)
+'''
+str[字符串]
+Number[数字]
+    整数  int 
+    浮点数 float 
+    复数 complex  
+bool[布尔值]
+None[空值]
+list[列表]
+tuple[元组]
+dict[字典]
+set[集合]
+'''
---
+++ HelloVar.py
@@ -0,0 +1,24 @@
+print("HelloWorld!")
+
+msg ="德玛西亚"
+
+msg2 = "德莱联盟"
+
+print(msg,msg2)
+
+msg2 = "英雄杀"
+
+print(msg2)
+
+msg3 ="我最喜欢学Python"
+#变量使用前先进行定义
+print(msg3)
+
+#删除变量
+del msg3
+#删除的变量不可以在被使用
+# print(msg3)
+#input 输入变量值
+msg5 = input("请输入消息：")
+
+print("msg5=",msg5)
